9514fe7051835fc7e5213b70b8590bd5dc74accb
Ashishs799 <np03cs4s220007@heraldcollege.edu.np>
2023-04-27 23:04:21 +0545

    updated

--- app/forms.py
+++ app/forms.py
@@ -8,15 +8,15 @@ from django.contrib.auth import password_validation
 # Creating forms for login and registration view
 class CustomerRegistrationForm(UserCreationForm):
     email = forms.CharField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 
-class Meta:
-    model = User
-    fields = ['username','email','password1','password2']
-    labels = {'email':'Email'}
-    widgets = {'username': forms.TextInput(attrs={'class':'form-control'})}
+    class Meta:
+        model = User
+        fields = ['username','email', 'password1','password2']
+        labels = {'email':'Email'}
+        widgets = {'username': forms.TextInput(attrs={'class':'form-control'})}
 
 class LoginForm(AuthenticationForm):
     username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True, 'class':'form-control'}))
